aec81fae28e465b8fcc9c1872ccec42a8454118e
Goran Minov <goran.s.minov@gmail.com>
2018-01-07 18:05:31 +0000

    Reformat code

--- app/src/main/java/com/lineargs/watchnext/ui/SeriesTopFragment.java
+++ app/src/main/java/com/lineargs/watchnext/ui/SeriesTopFragment.java
@@ -1,33 +1,16 @@
 package com.lineargs.watchnext.ui;
 
 import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import com.lineargs.watchnext.R;
 import com.lineargs.watchnext.adapters.SeriesTopAdapter;
 import com.lineargs.watchnext.data.DataContract;
-import com.lineargs.watchnext.data.Query;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
 /**
  * Created by goranminov on 04/11/2017.
