2fc431b6b5a79a1907f789d99feafddcea00b726
lineargs <goran.s.minov@gmail.com>
2018-09-07 20:01:10 +0100

    feat: Added series Entity

---
+++ app/src/main/java/com/lineargs/watchnext/data/Series.java
@@ -0,0 +1,183 @@
+package com.lineargs.watchnext.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "series")
+public class Series {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "tmdb_id")
+    private int tmdbId;
+
+    @ColumnInfo(name = "vote_average")
+    private String voteAverage;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "poster_path")
+    private String posterPath;
+
+    @ColumnInfo(name = "backdrop_path")
+    private String backdropPath;
+
+    @NonNull
+    @ColumnInfo(name = "overview")
+    private String overview;
+
+    @ColumnInfo(name = "release_date")
+    private String releaseDate;
+
+    @ColumnInfo(name = "homepage")
+    private String homepage;
+
+    @ColumnInfo(name = "production_companies")
+    private String productionCompanies;
+
+    @ColumnInfo(name = "networks")
+    private String networks;
+
+    @ColumnInfo(name = "runtime")
+    private int runtime;
+
+    @ColumnInfo(name = "status")
+    private String status;
+
+    @ColumnInfo(name = "genres")
+    private String genres;
+
+    @NonNull
+    @ColumnInfo(name = "type")
+    private int type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(int tmdbId) {
+        this.tmdbId = tmdbId;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    @NonNull
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(@NonNull String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public String getProductionCompanies() {
+        return productionCompanies;
+    }
+
+    public void setProductionCompanies(String productionCompanies) {
+        this.productionCompanies = productionCompanies;
+    }
+
+    public String getNetworks() {
+        return networks;
+    }
+
+    public void setNetworks(String networks) {
+        this.networks = networks;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
