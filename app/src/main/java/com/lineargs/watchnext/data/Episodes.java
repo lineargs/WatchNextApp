ce7c82eba4caa8bdca27f2304ae6346e05ef2ebc
lineargs <goran.s.minov@gmail.com>
2018-09-23 13:41:05 +0100

    feat: Added episodes Entity

---
+++ app/src/main/java/com/lineargs/watchnext/data/Episodes.java
@@ -0,0 +1,167 @@
+package com.lineargs.watchnext.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "episodes")
+public class Episodes {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "tmdb_id")
+    private int tmdbId;
+
+    @ColumnInfo(name = "season_id")
+    private int seasonId;
+
+    @ColumnInfo(name = "episode_number")
+    private int episodeNumber;
+
+    @ColumnInfo(name = "release_date")
+    private String releaseDate;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "overview")
+    private String overview;
+
+    @ColumnInfo(name = "season_number")
+    private int seasonNumber;
+
+    @ColumnInfo(name = "still_path")
+    private String stillPath;
+
+    @ColumnInfo(name = "vote_average")
+    private String voteAverage;
+
+    @ColumnInfo(name = "directors")
+    private String directors;
+
+    @ColumnInfo(name = "writers")
+    private String writers;
+
+    @ColumnInfo(name = "guest_stars")
+    private String guestStars;
+
+    @ColumnInfo(name = "vote_count")
+    private int voteCount;
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public int getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(int tmdbId) {
+        this.tmdbId = tmdbId;
+    }
+
+    public int getSeasonId() {
+        return seasonId;
+    }
+
+    public void setSeasonId(int seasonId) {
+        this.seasonId = seasonId;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public String getStillPath() {
+        return stillPath;
+    }
+
+    public void setStillPath(String stillPath) {
+        this.stillPath = stillPath;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public String getWriters() {
+        return writers;
+    }
+
+    public void setWriters(String writers) {
+        this.writers = writers;
+    }
+
+    public String getGuestStars() {
+        return guestStars;
+    }
+
+    public void setGuestStars(String guestStars) {
+        this.guestStars = guestStars;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+}
