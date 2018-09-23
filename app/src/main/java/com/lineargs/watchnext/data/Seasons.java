8066d283dc0d8fec7e8f3b22b2dff913c85b74f4
lineargs <goran.s.minov@gmail.com>
2018-09-23 13:27:23 +0100

    feat: Added seasons Entity

---
+++ app/src/main/java/com/lineargs/watchnext/data/Seasons.java
@@ -0,0 +1,90 @@
+package com.lineargs.watchnext.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "seasons")
+public class Seasons {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "tmdb_id")
+    private int tmdbId;
+
+    @ColumnInfo(name = "season_id")
+    private int seasonId;
+
+    @ColumnInfo(name = "episode_count")
+    private int episodeCount;
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public int getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(int tmdbId) {
+        this.tmdbId = tmdbId;
+    }
+
+    public int getSeasonId() {
+        return seasonId;
+    }
+
+    public void setSeasonId(int seasonId) {
+        this.seasonId = seasonId;
+    }
+
+    public int getEpisodeCount() {
+        return episodeCount;
+    }
+
+    public void setEpisodeCount(int episodeCount) {
+        this.episodeCount = episodeCount;
+    }
+
+    public String getShowName() {
+        return showName;
+    }
+
+    public void setShowName(String showName) {
+        this.showName = showName;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(String seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    @ColumnInfo(name = "show_name")
+    private String showName;
+
+    @ColumnInfo(name = "poster_path")
+    private String posterPath;
+
+    @ColumnInfo(name = "season_number")
+    private String seasonNumber;
+}
