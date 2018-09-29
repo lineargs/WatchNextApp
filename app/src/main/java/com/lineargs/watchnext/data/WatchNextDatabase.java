d7612272c660d84b9d289b47642e047f1c698ac1
lineargs <goran.s.minov@gmail.com>
2018-09-29 19:03:32 +0100

    feat: Created Room Database

---
+++ app/src/main/java/com/lineargs/watchnext/data/WatchNextDatabase.java
@@ -0,0 +1,31 @@
+package com.lineargs.watchnext.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Credits.class, Episodes.class, Favourites.class,
+        Movies.class, Person.class, Reviews.class,
+        Seasons.class, Series.class, Videos.class}, version = 1)
+public abstract class WatchNextDatabase extends RoomDatabase {
+
+    private static volatile WatchNextDatabase INSTANCE;
+
+    static WatchNextDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WatchNextDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WatchNextDatabase.class, "watchnextdb")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract MoviesDao moviesDao();
+
+    public abstract SeriesDao seriesDao();
+}
