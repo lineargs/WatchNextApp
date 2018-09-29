c0592b5587a88c0200c9632221701d6de4e9ec78
lineargs <goran.s.minov@gmail.com>
2018-09-29 18:46:08 +0100

    feat: Added Movies Dao

---
+++ app/src/main/java/com/lineargs/watchnext/data/MoviesDao.java
@@ -0,0 +1,35 @@
+package com.lineargs.watchnext.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MoviesDao {
+
+    @Insert
+    void insert(Movies movies);
+
+    @Insert
+    void insertAll(List<Movies> movies);
+
+    @Query("DELETE FROM movies")
+    void deleteAll();
+
+    @Query("SELECT * FROM movies WHERE type = 0 ORDER BY title ASC")
+    List<Movies> getPopularMovies();
+
+    @Query("SELECT * FROM movies WHERE type = 1 ORDER BY title ASC")
+    List<Movies> getTopratedMovies();
+
+    @Query("SELECT * FROM movies WHERE type = 2 ORDER BY title ASC")
+    List<Movies> getUpcomingMovies();
+
+    @Query("SELECT * FROM movies WHERE type = 3 ORDER BY title ASC")
+    List<Movies> getTheatreMovies();
+
+    @Query("SELECT * FROM movies ORDER BY title ASC")
+    List<Movies> getAllMovies();
+}
