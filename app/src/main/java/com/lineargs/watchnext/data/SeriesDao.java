b7430753b94e42cceb3f6ef72c38f61e2a46d934
lineargs <goran.s.minov@gmail.com>
2018-09-29 19:50:25 +0100

    refactor: Annotated SeriesDao

--- app/src/main/java/com/lineargs/watchnext/data/SeriesDao.java
+++ app/src/main/java/com/lineargs/watchnext/data/SeriesDao.java
@@ -1,11 +1,13 @@
 package com.lineargs.watchnext.data;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface SeriesDao {
 
     @Insert
